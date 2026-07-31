538683d956e49fd43b0f0817b647300f056b56cf
jakkubiradar143Biradar <jakkubiradar143Biradar@users.noreply.github.com>
2026-07-31 19:13:12 +0530

    Serve static files from both public and root directory

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Upload & Temp storage
 const uploadDir = path.join(__dirname, 'uploads');
